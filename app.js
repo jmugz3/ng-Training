60073099e0f1c8e73eb4ceaaa1295c64e26316ca
unknown <jose.mugaburu@WSTLH000265.na.aderant.com>
2015-02-24 17:45:50 -0500

    Adds route parameters to the forecast page

--- app.js
+++ app.js
@@ -14,7 +14,13 @@ ratingApp.config(function($routeProvider){
         templateUrl: 'pages/forecast.htm',
         controller: 'forecastController'
     })
-
+    
+    .when('/forecast/:days',{
+        templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    })
+    
+    
 });
 
 
@@ -36,14 +42,28 @@ ratingApp.controller('homeController', ['$scope','cityService', function($scope,
     });
 }]);
 
-ratingApp.controller('forecastController',['$scope','$resource','cityService',function($scope,$resource,cityService){
+ratingApp.controller('forecastController',['$scope','$resource', '$routeParams','cityService',function($scope,$resource,$routeParams,cityService){
 
     $scope.city = cityService.city;
+    $scope.days = $routeParams.days || '2';
+    
     $scope.ratingAPI = 
         $resource("http://api.openweathermap.org/data/2.5/forecast/daily",{
-        callback: "JSON_CALLBACK"} , {get: {method: "JSONP" }});
+        callback: "JSON_CALLBACK"}, {get: {method: "JSONP" }});
     
-    $scope.weatherResult = $scope.ratingAPI.get
+    $scope.weatherResult = $scope.ratingAPI.get({q: $scope.city, cnt:$scope.days});
+    
+    //console.log($scope.weatherResult);
+    
+    
+    $scope.convertToFahrenheit= function(degK){
+        return Math.round((1.8*(degK -273)) + 32);
+    
+    }
+    
+    $scope.convertToDate = function(dt){
+        return new Date(dt * 1000);
+    }
     
     
 }]);
